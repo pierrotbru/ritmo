b333a1eac1258989362cefcaa819f551cf33b59d
KSENIA\ECiarrocchi <eciarrocchi@kseniasecurity.com>
2025-02-28 12:10:24 +0100

    contents_add
    L'inserimento di nuovi contents è completo

--- src/db/adds/add_languages.rs
+++ src/db/adds/add_languages.rs
@@ -1,3 +1,4 @@
+use sqlx::query;
 use crate::db::adds::search_and_add::search_and_add;
 use crate::db::adds::search_and_add::IdAction;
 use crate::ContentData;
@@ -35,37 +36,53 @@ pub async fn get_language_code_by_name(
 pub async fn add_languages(
     tx: &mut Transaction<'_, Sqlite>,
     new_content: &ContentData,
-    _new_content_id: i64, // Usa i64 invece di u64
+    new_content_id: i64, // Usa i64 invece di u64
 ) -> Result<(), RitmoErr> {
-
-    println!("new_content.curr_lang {:?}", &new_content.curr_lang);
-
     for curr in &new_content.curr_lang {
         let code = get_language_code_by_name(tx, curr).await?; // Aspetta il risultato e gestisci l'errore
-
-        println!("{:?}",code );
         let curr_result = search_and_add(tx, "current_languages", "id", "iso_code", &code, IdAction::AddId,).await
         .map_err(|e| RitmoErr::SearchAndAddFailed(format!("Failed to search and add current language {}: {}", curr, e)))?;
+        query!(
+            "INSERT INTO contents_current_languages (content_id, curr_lang_id) VALUES (?, ?)",
+            new_content_id,
+            curr_result.id
+        )
+        .execute(&mut **tx)
+        .await
+        .map_err(|e| {
+            RitmoErr::DatabaseInsertFailed(format!("Failed to insert into contents_current_languages: {}", e))
+        })?;
 
-        println!("{:?}",curr_result );
     }
     for curr in &new_content.src_lang {
         let code = get_language_code_by_name(tx, curr).await?; // Aspetta il risultato e gestisci l'errore
-
-        println!("{:?}",code );
         let curr_result = search_and_add(tx, "source_languages", "id", "iso_code", &code, IdAction::AddId,).await
-        .map_err(|e| RitmoErr::SearchAndAddFailed(format!("Failed to search and add current language {}: {}", curr, e)))?;
-
-        println!("{:?}",curr_result );
+        .map_err(|e| RitmoErr::SearchAndAddFailed(format!("Failed to search and add source language {}: {}", curr, e)))?;
+        query!(
+            "INSERT INTO contents_source_languages (content_id, source_lang_id) VALUES (?, ?)",
+            new_content_id,
+            curr_result.id
+        )
+        .execute(&mut **tx)
+        .await
+        .map_err(|e| {
+            RitmoErr::DatabaseInsertFailed(format!("Failed to insert into contents_source_languages: {}", e))
+        })?;
     }
     for curr in &new_content.orig_lang {
         let code = get_language_code_by_name(tx, curr).await?; // Aspetta il risultato e gestisci l'errore
-
-        println!("{:?}",code );
         let curr_result = search_and_add(tx, "original_languages", "id", "iso_code", &code, IdAction::AddId,).await
-        .map_err(|e| RitmoErr::SearchAndAddFailed(format!("Failed to search and add current language {}: {}", curr, e)))?;
-
-        println!("{:?}",curr_result );
+        .map_err(|e| RitmoErr::SearchAndAddFailed(format!("Failed to search and add original language {}: {}", curr, e)))?;
+        query!(
+            "INSERT INTO contents_original_languages (content_id, orig_lang_id) VALUES (?, ?)",
+            new_content_id,
+            curr_result.id
+        )
+        .execute(&mut **tx)
+        .await
+        .map_err(|e| {
+            RitmoErr::DatabaseInsertFailed(format!("Failed to insert into contents_original_languages: {}", e))
+        })?;
     }
 
     Ok(())
